c8c928027f2b5c433522a4c2e37a34bff16230d3
ryujiin <ryujiin22@gmail.com>
2016-11-29 16:01:31 -0500

    url

--- django_project/urls.py
+++ django_project/urls.py
@@ -34,6 +34,22 @@ router.register(r'comentarioimgs',ComentarioImagenViewSet,'comentarios_imagenes'
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(router.urls)),
+    url(r'^api/carro/', include('carro.urls')),       
+    #Usauario 
+    url(r'^api/user/', include('cliente.urls')),        
+    url(r'^ajax/crear/', nuevo_usuario, name='nuevo_usuario'),    
+    url(r'^salir/$',salir,name='salir'),
+    url(r'^login/$',ingresar,name='salir'),
+    #pagos
+    url(r'^pago_contraentrega/',get_pago_contraentrega,name='pago_contraentrega'),    
+    url(r'^definir_pago/',definir_pago,name='definir_pago'),    
+    url(r'^retorno_paypal/',retorn_paypal,name='retorn_paypal'),    
+    url(r'^pago/stripe/$',stripe_paymet,name='pago_stripe'),
+    url(r'^get_stripe_key/$',get_stripe_key,name='get_key'),    
+    url(r'^pago/paypal/', paypal_paymet,name = 'pago_paypal'),    
+    url(r'^hardcode/get/paypal/', include('paypal.standard.ipn.urls')),
+    #Web
+    url(r'^',include('cmsweb.urls')),
 
 ]
 if settings.DEBUG:
